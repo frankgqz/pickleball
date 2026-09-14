359b343c84b8acf061bb36e1b726a16bd1e14524
frankgqz <52376597+frankgqz@users.noreply.github.com>
2026-09-14 17:37:51 +1000

    provider

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { ThemeProvider } from '@/app/useTheme'
+import { ThemeProvider } from '@/app/ThemeProvider'
 
 
 export default function Providers({ children }: { children: React.ReactNode }) {
